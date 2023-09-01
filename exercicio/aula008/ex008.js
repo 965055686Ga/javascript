cc9c28f004abbc904da9ffd72fa3879b8fae2f49
Gabrielvieiraez <123672390+965055686Ga@users.noreply.github.com>
2023-09-01 05:50:39 -0300

    console

---
+++ exercicio/aula008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 72
+console.log(`A volocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapaçou a volocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando sinto de segurança`)
