438424551e0658e0426696cb297fa7990fbbb37d
Gabrielvieiraez <123672390+965055686Ga@users.noreply.github.com>
2023-09-18 20:57:02 -0300

    projeto javascrip

---
+++ exercicio/aula010/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // Bom dia
+        img.src = 'fotomanha.jpg'
+        document.body.style.backgroundColor = '#d6be34'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = 'fototarde.jpg'
+        document.body.style.backgroundColor = '#913a00'
+    } else {
+        //Boa Noite
+        img.src = 'fotonoite.jpg'
+        document.body.style.backgroundColor = '#010829'
+    }
+}
+
+
