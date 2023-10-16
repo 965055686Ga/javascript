2a42b2b0c7a0304f4ba85108692f117cf024cbf5
Gabrielvieiraez <123672390+965055686Ga@users.noreply.github.com>
2023-10-16 18:25:23 -0300

    pratica java

--- exercicio/aula010/modelo/script.js
+++ exercicio/aula010/modelo/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var geneno = ''
+        if (fsex[0],checked) {
+            geneno = 'Homem'
+        } else if (fsex[1],checked) {
+            geneno = 'Mulher'
+        }
+           res.innerHTML = `Detectamos ${geneno} com ${idade} anos.` 
+    }
+}
